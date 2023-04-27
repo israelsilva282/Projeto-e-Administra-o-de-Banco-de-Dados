fe1f101105dd7fbccef177c2b6c986e211cc919e
israelsilva282 <israelcostas@hotmail.com>
2023-04-27 14:02:15 -0300

    Adicionado comentário informado o enunciado da questao (#1)

--- tarefas/t01/tarefa01-q1.sql
+++ tarefas/t01/tarefa01-q1.sql
@@ -1,3 +1,5 @@
+-- Faça uma consulta que selecione o nome dos funcionários que recebem salários superiores aos salários pagos no departamento 2.
+
 SELECT f.nome, f.salario
 FROM funcionario f
 WHERE f.salario > ANY (
